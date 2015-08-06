e85199ba3fc059e2e70a4763e10bcbafe542346b
leonard <leonard_henriquez@hotmail.com>
2015-08-06 04:36:04 +0200

    long arguments supported

--- main.cpp
+++ main.cpp
@@ -1,30 +1,45 @@
 #include <iostream>
 #include "multilayerperceptron.h"
 #include "io.h"
+#include <getopt.h>
 
 
 using namespace std;
 
-bool adr = 0;
+bool alr = 0;
 realnumber mT = 10, lR = 0.001;
-string structure, input, output = "./out.mlp", dataImage = "/home/leonard/MNIST/trainImages", dataLabel = "/home/leonard/MNIST/trainLabels";
+string structure, input, output, dataImage = "/home/leonard/MNIST/trainImages", dataLabel = "/home/leonard/MNIST/trainLabels";
 integer nbExamples = 2000;
 
 void arguments(int argc, char* argv[])
 {
-	integer opt;
-	while ( ( opt = getopt(argc, argv, "at:r:i:o:e:d:l:s:") ) != -1 )
+	static struct option long_options[] =
 	{
-		switch (opt)
+		{"alr", no_argument, NULL, 'a'},
+		{"lr", required_argument, NULL, 'r'},
+		{"time", required_argument, NULL, 't'},
+		{"input", required_argument, NULL, 'i'},
+		{"output", required_argument, NULL, 'o'},
+		{"image", required_argument, NULL, 'd'},
+		{"label", required_argument, NULL, 'l'},
+		{"example", required_argument, NULL, 'e'},
+		{"struct", required_argument, NULL, 's'},
+		{NULL, 0, NULL, 0}
+	};
+
+	integer c;
+	while ( ( c = getopt_long(argc, argv, "at:r:i:o:e:d:l:s:", long_options, NULL) ) != -1 )
+	{
+		switch (c)
 		{
 		case 'a':
-			adr = 1;
-			break;
-		case 't':
-			mT = atof(optarg);
+			alr = 1;
 			break;
 		case 'r':
 			lR = atof(optarg);
+			break;
+		case 't':
+			mT = atof(optarg);
 			break;
 		case 'i':
 			input = optarg;
@@ -61,14 +76,14 @@ int main(int argc, char* argv[])
 
 	mlp.setLearningData(data);
 	learningParameters parameters;
-	parameters.adaptativeLearningRate = adr;
+	parameters.adaptativeLearningRate = alr;
 	parameters.learningRate = lR;
 	parameters.maxTime = mT;
 
 	if ( !structure.empty() )
 	{
 		vector<integer> str;
-        str.push_back(data.inputs());
+		str.push_back( data.inputs() );
 		integer i = structure.find(",");
 		while (i != string::npos)
 		{
@@ -76,8 +91,8 @@ int main(int argc, char* argv[])
 			structure = structure.substr(i + 1);
 			i = structure.find(",");
 		}
-        str.push_back( stoi(structure) );
-        str.push_back(data.outputs());
+		str.push_back( stoi(structure) );
+		str.push_back( data.outputs() );
 
 		mlp.setStructure(str);
 	}
@@ -88,7 +103,8 @@ int main(int argc, char* argv[])
 
 	mlp.gradientDescent(parameters);
 
-	writeMLP(output, mlp);
+	if ( !output.empty() )
+		writeMLP(output, mlp);
 
 	return 0;
 }
