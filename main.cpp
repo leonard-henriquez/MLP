17be9c7f2f22a0a9bba248ac37e5a9c8f737972a
leonard <leonard_henriquez@hotmail.com>
2015-08-06 02:57:44 +0200

    minor fixes

--- main.cpp
+++ main.cpp
@@ -7,13 +7,13 @@ using namespace std;
 
 bool adr = 0;
 realnumber mT = 10, lR = 0.001;
-string input, output = "./out.mlp", dataImage = "/home/leonard/MNIST/trainImages", dataLabel = "/home/leonard/MNIST/trainLabels";
+string structure, input, output = "./out.mlp", dataImage = "/home/leonard/MNIST/trainImages", dataLabel = "/home/leonard/MNIST/trainLabels";
 integer nbExamples = 2000;
 
 void arguments(int argc, char* argv[])
 {
 	integer opt;
-    while ( ( opt = getopt(argc, argv, "at:r:i:o:e:d:l:") ) != -1 )
+	while ( ( opt = getopt(argc, argv, "at:r:i:o:e:d:l:s:") ) != -1 )
 	{
 		switch (opt)
 		{
@@ -23,7 +23,7 @@ void arguments(int argc, char* argv[])
 		case 't':
 			mT = atof(optarg);
 			break;
-        case 'r':
+		case 'r':
 			lR = atof(optarg);
 			break;
 		case 'i':
@@ -32,14 +32,17 @@ void arguments(int argc, char* argv[])
 		case 'o':
 			output = optarg;
 			break;
-        case 'd':
-            dataImage = optarg;
-            break;
-        case 'l':
-            dataLabel = optarg;
-            break;
+		case 'd':
+			dataImage = optarg;
+			break;
+		case 'l':
+			dataLabel = optarg;
+			break;
 		case 'e':
 			nbExamples = atoi(optarg);
+			break;
+		case 's':
+			structure = optarg;
 			break;
 		}
 	}
@@ -48,13 +51,13 @@ void arguments(int argc, char* argv[])
 
 int main(int argc, char* argv[])
 {
-    cout << "\033[1;1H\x1b[2J";
+	cout << "\033[1;1H\x1b[2J";
 
-    arguments(argc, argv);
+	arguments(argc, argv);
 
 	MLP mlp;
 
-    learningData data( readMNISTPics(dataImage, nbExamples), readMNISTLabels(dataLabel, nbExamples) );
+	learningData data( readMNISTPics(dataImage, nbExamples), readMNISTLabels(dataLabel, nbExamples) );
 
 	mlp.setLearningData(data);
 	learningParameters parameters;
@@ -62,12 +65,26 @@ int main(int argc, char* argv[])
 	parameters.learningRate = lR;
 	parameters.maxTime = mT;
 
-	if ( !input.empty() )
+	if ( !structure.empty() )
+	{
+		vector<integer> str;
+        str.push_back(data.inputs());
+		integer i = structure.find(",");
+		while (i != string::npos)
+		{
+			str.push_back( stoi( structure.substr(0, i) ) );
+			structure = structure.substr(i + 1);
+			i = structure.find(",");
+		}
+        str.push_back( stoi(structure) );
+        str.push_back(data.outputs());
+
+		mlp.setStructure(str);
+	}
+	else if ( !input.empty() )
 		readMLP(input, mlp);
 	else
 		mlp.setStructure({784, 2000, 1500, 1000, 500, 10});
-
-    cout << endl;
 
 	mlp.gradientDescent(parameters);
 
